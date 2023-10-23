9396b56632dbdd132aa06fa4284f93a3ffe1cbb7
zmaderow <trashypg@gmail.com>
2023-10-23 15:21:34 -0300

    :tada: initial commit

---
+++ assets/script/index.js
@@ -0,0 +1,51 @@
+(function(){
+
+    window.sr = ScrollReveal();
+
+    if ($(window).width() < 768) {
+
+        if ($('.timeline').hasClass('revealLeft')) {
+            $('.timeline').removeClass('revealLeft').addClass('revealRight');
+        }
+
+        sr.reveal('.revealRight', {
+            origin: 'right',
+            distance: '300px',
+            easing: 'ease-in-out',
+            duration: 800,
+        });
+
+    } else {
+
+        sr.reveal('.revealLeft', {
+            origin: 'left',
+            distance: '300px',
+            easing: 'ease-in-out',
+            duration: 800,
+        });
+
+        sr.reveal('.revealRight', {
+            origin: 'right',
+            distance: '300px',
+            easing: 'ease-in-out',
+            duration: 800,
+        });
+
+    }
+
+    sr.reveal('.revealLeft', {
+        origin: 'left',
+        distance: '300px',
+        easing: 'ease-in-out',
+        duration: 800,
+    });
+
+    sr.reveal('.revealRight', {
+        origin: 'right',
+        distance: '300px',
+        easing: 'ease-in-out',
+        duration: 800,
+    });
+
+
+});
